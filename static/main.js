1f8594552241f6f82833e88358a286086e7dc9ef
Not-A-Problem <96020643+Not-A-Problem@users.noreply.github.com>
2022-04-22 15:34:17 +0530

    Add files via upload

---
+++ static/main.js
@@ -0,0 +1,26 @@
+console.log("HELLO WORLD")
+
+function PreviewImage() {
+    var oFReader = new FileReader();
+
+    oFReader.readAsDataURL(document.getElementById("uploadImage").files[0]);
+    oFReader.onload = function (oFREvent) {
+        document.getElementById("uploadPreview").src = oFREvent.target.result;
+        const scrollingElement = (document.scrollingElement || document.body);
+        scrollingElement.scrollTop = scrollingElement.scrollHeight;
+    };
+};
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("menu_line").style.display = "none"
+};
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("menu_line").style.display = "block"
+};
+
+
