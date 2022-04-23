55201e2e4ee082dbf17e16b4f021618bf8dafa0f
Not-A-Problem <96020643+Not-A-Problem@users.noreply.github.com>
2022-04-23 20:45:45 +0530

    Update inf.py

--- inf.py
+++ inf.py
@@ -1,20 +1,16 @@
 import numpy as np
 import tensorflow as tf
-import cv2
 
 
 def predict(image):
 
     clases = ['Early_blight', 'Late_blight', 'Healthy']
-# Load the TFLite model and allocate tensors.
     interpreter = tf.lite.Interpreter(model_path="model.tflite")
     interpreter.allocate_tensors()
 
-# Get input and output tensors.
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
-# Test the model on random input data.
     input_shape = input_details[0]['shape']
     print(input_shape)
     print("*"*50, input_details)
@@ -22,8 +18,6 @@ def predict(image):
 
     interpreter.invoke()
 
-# The function `get_tensor()` returns a copy of the tensor data.
-# Use `tensor()` in order to get a pointer to the tensor.
     output_data = interpreter.get_tensor(output_details[0]['index'])
     confidence = round((output_data[0][np.argmax(output_data)]) * 100, 3)
     return clases[np.argmax(output_data)], confidence
